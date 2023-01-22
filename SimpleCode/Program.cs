c596040f4303fbc45297cfd804010b35772144a1
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-01-22 14:12:59 +0600

    Простая система маршрутизации

--- SimpleCode/Program.cs
+++ SimpleCode/Program.cs
@@ -22,7 +22,14 @@ app.MapRazorPages();
 
 app.Run(async (context) => 
     {
-        await context.Response.WriteAsync($"Path: {context.Request.Path}");
+        var path = context.Request.Path;
+        var now = DateTime.Now;
+        var response = context.Response;
+
+        if (path == "/date") await response.WriteAsync($"Date: {now.ToShortDateString()}");
+        else if (path == "/time") await response.WriteAsync($"Time: {now.ToShortTimeString()}");
+        else await response.WriteAsync("Hello Metanit");
+
     }
 );
 
